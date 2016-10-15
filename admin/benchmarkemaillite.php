3cded9e514d50710dadddea2696dffaf78ece1c5
Sean Conklin <user@iMac.local>
2016-10-14 23:12:42 -0700

    Improvements to Joomla3 admin UI controls

--- admin/benchmarkemaillite.php
+++ admin/benchmarkemaillite.php
@@ -4,14 +4,16 @@
  This file is the main admin controller used by Joomla.
 *******************************************************/
 
-// Admin Tool Bar
-JToolBarHelper::title( 'Benchmark Email Lite', 'envelope' );
-JToolBarHelper::preferences( 'com_benchmarkemaillite' );
-
 // Load Component Translations
 $language = JFactory::getLanguage();
 $language_tag = $language->getTag();
 $language->load( 'com_benchmarkemaillite.sys', dirname(__FILE__), $language_tag, true );
+
+// Admin Tool Bar
+JToolBarHelper::title( 'Benchmark Email Lite', 'envelope' );
+JToolBarHelper::custom( 'add-module', 'new', 'new', 'COM_BENCHMARKEMAILLITE_MODULE_ADD' );
+JToolBarHelper::custom( 'edit-module', 'edit', 'edit', 'COM_BENCHMARKEMAILLITE_MODULE_EDIT' );
+JToolBarHelper::preferences( 'com_benchmarkemaillite' );
 
 // Joomla WP Bootup
 require_once( 'controllers/joomla-bootup.php' );
@@ -23,26 +25,41 @@ benchmarkemaillite_api::handshake( array( benchmarkemaillite_api::$token ) );
 if( ! benchmarkemaillite_api::$token ) {
 ?>
 
-<h2>Setup Instructions:</h2>
-
-<ol>
-	<li><?php _e( 'COM_BENCHMARKEMAILLITE_CONFIG1_DESC1' ); ?></li>
-	<li><?php _e( 'COM_BENCHMARKEMAILLITE_CONFIG1_DESC2' ); ?></li>
-</ol>
+<div class="alert alert-info">
+	<span class="icon-info"></span>
+	<a href='index.php?option=com_config&view=component&component=com_benchmarkemaillite'>
+		<?php _e( 'COM_BENCHMARKEMAILLITE_CONFIG1_DESC2' ); ?>
+	</a>
+</div>
 
 <?php
 }
 
-// Look Up Their Lists And Forms
+// Has API Key
 else {
+
+	// Handle Requests
+	$task = isset( $_REQUEST['task'] ) ? $_REQUEST['task'] : '';
+	switch( $task ) {
+		case 'add-module':
+			header( 'Location: index.php?option=com_modules&view=select' );
+			exit;
+		case 'edit-module':
+			header( 'Location: index.php?option=com_modules' );
+			exit;
+	}
+
+	// Look Up Their Lists And Forms
 	$contact_lists = benchmarkemaillite_api::lists();
 	$signup_forms = benchmarkemaillite_api::signup_forms();
 ?>
 
-<div class="alert alert-info">
-	<span class="icon-info"></span>
-	<?php _e( 'COM_BENCHMARKEMAILLITE_CONFIG1_DESC4' ); ?>
-</div>
+<form action="index.php" method="post" name="adminForm" id="adminForm">
+	<input type="hidden" name="option" value="com_benchmarkemaillite" />
+	<input type="hidden" name="boxchecked" value="1" />
+	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="controller" value="benchmarkemaillite" />
+</form>
 
 <h2><?php _e( 'COM_BENCHMARKEMAILLITE_HEADING_LISTS' ); ?></h2>
 

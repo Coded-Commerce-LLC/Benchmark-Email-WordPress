5febce4603fa9a2024a2bf41c5df2f2d7e9fc85c
Sean Conklin <user@iMac.local>
2016-10-18 19:29:11 -0700

    Several updates and minor fixes pre launch Joomla first release

--- component/controllers/joomla-wp-bootup.php
+++ component/controllers/joomla-wp-bootup.php
@@ -1,26 +1,22 @@
 <?php
 
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// Define Plugin Path
+define( 'BMEL_DIR_PATH', JPATH_ADMINISTRATOR . '/components/com_benchmarkemaillite/' );
+define( 'BMEL_DIR_URL', JURI::base() . 'administrator/components/com_benchmarkemaillite/' );
+
 // Stand-Ins For WordPress Core Functions
 if( ! function_exists( 'get_option' ) ) {
 	function get_option( $arg ) {
-		switch( $arg ) {
-			case 'widget_benchmarkemaillite_widget':
-				break;
-			default:
-				return array();
-		}
+		return array();
 	}
 }
 if( ! function_exists( 'get_transient' ) ) {
 	function get_transient( $arg ) { }
 }
 if( ! function_exists( 'set_transient' ) ) {
-	function set_transient( $arg1, $arg2, $arg3 ) {
-		switch( $arg1 ) {
-			case 'benchmark-email-lite_log':
-				break;
-		}
-	}
+	function set_transient( $arg1, $arg2, $arg3 ) { }
 }
 if( ! function_exists( 'current_time' ) ) {
 	function current_time( $arg1, $arg2=0 ) {
@@ -55,7 +51,7 @@ if( ! function_exists( 'sanitize_title' ) ) {
 }
 if( ! function_exists( 'esc_attr' ) ) {
 	function esc_attr( $arg1 ) {
-		return $arg1;
+		return htmlentities( $arg1, ENT_QUOTES );
 	}
 }
 if( ! function_exists( 'wp_get_current_user' ) ) {
@@ -69,27 +65,23 @@ if( ! function_exists( 'wp_get_current_user' ) ) {
 	}
 }
 if( ! function_exists( 'is_email' ) ) {
-	function is_email() {
-		return true;
+	function is_email( $arg1 ) {
+		return JMailHelper::isEmailAddress( $arg1 );
 	}
 }
-
-// Define Plugin Path
-define( 'BMEL_DIR_PATH', JPATH_ADMINISTRATOR . '/components/com_benchmarkemaillite/' );
-define( 'BMEL_DIR_URL', JURI::base() . 'administrator/components/com_benchmarkemaillite/' );
+if( ! class_exists( 'WP_Widget' ) ) {
+	class WP_Widget {
+		function __construct( $arg1, $arg2, $arg3, $arg4 ) { }
+	}
+}
 
 // Include XML-RPC Client
 require_once( BMEL_DIR_PATH . 'assets/lib/class-IXR.php' );
 
-// Include Models
+// Include Models Used By Joomla!
 require_once( BMEL_DIR_PATH . 'models/class.api.php' );
 require_once( BMEL_DIR_PATH . 'models/class.widget.php' );
 
 // Set API Key From Settings
 $benchmarkemaillite_params = JComponentHelper::getParams( 'com_benchmarkemaillite' );
 benchmarkemaillite_api::$token = $benchmarkemaillite_params->get( 'api_keys' );
-
-// Stand In For WordPress Code Widget
-class WP_Widget {
-	function __construct( $arg1, $arg2, $arg3, $arg4 ) { }
-}

363176fbe669c0129abc08d32c8f99b7b6d8aff7
Sean Conklin <seanconklin@yahoo.com>
2016-10-20 15:53:47 -0700

    Added Joomla3 package builder to combine component and module into a single installation ZIP

--- build_scripts/joomla3.sh
+++ build_scripts/joomla3.sh
@@ -1,11 +1,21 @@
 cd ..;
 git pull;
 git status;
+
+mkdir packages;
+
 cd joomla-module;
-zip -x "*.git*" -x "*.DS_Store" -r ~/joomla-module.zip *;
+zip -x "*.git*" -x "*.DS_Store" -r ../packages/mod_benchmarkemaillite.zip *;
+
 cd ..;
-zip -x "*.git*" -x "*.DS_Store" -r ~/joomla-component.zip \
+zip -x "*.git*" -x "*.DS_Store" -r packages/com_benchmarkemaillite.zip \
 component \
 index.html \
 joomla-component.xml \
 license.txt;
+
+zip -r ~/pkg_benchmarkemaillite.zip pkg_benchmarkemaillite.xml packages;
+
+rm packages/com_benchmarkemaillite.zip;
+rm packages/mod_benchmarkemaillite.zip;
+rmdir packages;
